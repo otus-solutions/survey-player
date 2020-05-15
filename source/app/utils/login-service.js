d1282d4f5be39be8bc136d5c0f5a63212e6abecf
FlaviaAP <flavia.avila@ufrgs.br>
2020-05-15 09:37:41 -0300

    SP-9 pequenas refatorações

--- source/app/utils/login-service.js
+++ source/app/utils/login-service.js
@@ -28,15 +28,14 @@
 
     function isAuthenticated() {
       var _token = SurveyApiService.getAuthToken();
-      return _token && $rootScope.online ? true : false;
+      return (_token && $rootScope.online);
     }
 
     function authenticate() {
       if (!self.isAuthenticated()) {
         return _login();
-      } else {
-        return _logout();
       }
+      return _logout();
     }
 
     function _login() {
