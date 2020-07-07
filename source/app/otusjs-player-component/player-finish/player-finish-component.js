d38ce78c966e71219983a32cb0c3d5b11d3310f4
FlaviaAP <flavia.avila@ufrgs.br>
2020-07-07 17:23:37 -0300

    SP-15 palyer-finish apontando para survey-back-cover2

--- source/app/otusjs-player-component/player-finish/player-finish-component.js
+++ source/app/otusjs-player-component/player-finish/player-finish-component.js
@@ -8,38 +8,9 @@
       controller: Controller
     });
 
-  Controller.$inject = [
-    'otusjs.player.core.player.PlayerService'
-  ];
+  Controller.$inject = [];
 
-  function Controller(PlayerService) {
-    var self = this;
-
-    /* Public methods */
-    self.$onInit = onInit;
-    self.eject = eject;
-    self.stop = stop;
-
-
-    function onInit() {
-      /*
-       * These objects are initialized by child components of Player
-       * See player-commander-component.js (onInit method)
-       * See player-display-component.js (onInit method)
-       */
-      self.playerCommander = {};
-      self.playerDisplay = {};
-      self.playerCover = {};
-      self.playerBackCover = {};
-      PlayerService.bindComponent(self);
-    }
-
-    function eject() {
-      PlayerService.eject();
-    }
-
-    function stop() {
-      PlayerService.stop();
-    }
+  function Controller() {
+    let self = this;
   }
 }());
