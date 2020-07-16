669dd237c597dc3723760a2281b45ee9ef450484
FlaviaAP <flavia.avila@ufrgs.br>
2020-07-16 14:59:28 -0300

    SP-15 add método getCurrentSurvey em PlayerService
    util em componentes que já injetam PlayerService, mas injetam também ActivityFacadeService  só pata fazer a chamada pela survey

--- source/app/otusjs-player-core/player/player-service.js
+++ source/app/otusjs-player-core/player/player-service.js
@@ -46,6 +46,7 @@
     self.eject = eject;
     self.stop = stop;
     self.save = save;
+    self.getCurrentSurvey = getCurrentSurvey;
 
     /**/
     self.registerHardBlocker = registerHardBlocker;
@@ -127,6 +128,10 @@
 
     function save() {
       SaveActionService.execute();
+    }
+
+    function getCurrentSurvey(){
+      return ActivityFacadeService.getCurrentSurvey().getSurvey();
     }
   }
 })();
