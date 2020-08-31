f2d9f2d7333c00e65b34da108d5b7684b82b4207
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-31 11:43:43 -0300

    SP-20 tela de confirmação de participante

---
+++ source/app/otusjs-player-component/confirmation-participant/confirmation-participant-component.js
@@ -0,0 +1,72 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.player.component')
+    .component('otusConfirmationParticipant', {
+      controller: 'otusConfirmationParticipantCtrl as $ctrl',
+      templateUrl: 'app/otusjs-player-component/confirmation-participant/confirmation-participant-template.html'
+    }).controller('otusConfirmationParticipantCtrl', Controller);
+
+  Controller.$inject = [
+    '$state',
+    'STATE',
+    'otusjs.player.core.player.PlayerService'
+  ];
+
+  function Controller($state, STATE, PlayerService) {
+    let self = this;
+
+    /* Public methods */
+    self.$onInit = onInit;
+    self.play = play;
+    self.stop = stop;
+
+    function onInit() {
+      self.participantName = PlayerService.getCurrentSurvey().participantData.name;
+
+      _unblock();
+    }
+
+    function _unblock() {
+      self.hardError = false;
+      self.softError = false;
+      self.softProgress = false;
+      self.hardProgress = false;
+
+      if (self.hardBlocker) {
+        self.hardProgress = true;
+        self.hardBlocker
+          .then(function () {
+            self.hardProgress = false;
+          })
+          .catch(function () {
+            self.hardProgress = false;
+            self.hardError = true;
+            self.message = 'Ocorreu um erro ao baixar informações necessárias ao preenchimento da atividade. Clique para sair.';
+          });
+      }
+
+      if (self.softBlocker) {
+        self.softProgress = true;
+        self.softBlocker
+          .then(function () {
+            self.softProgress = false;
+          })
+          .catch(function () {
+            self.softProgress = false;
+            self.softError = true;
+          });
+      }
+    }
+
+    function play() {
+      $state.go(STATE.BEGIN);
+    }
+
+    function stop(){
+      PlayerService.stop();
+    }
+
+  }
+}());
