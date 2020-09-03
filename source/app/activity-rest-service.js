db506cb0f98c1bfbb6f54875a8a03a0bb7aaf738
FlaviaAP <flavia.avila@ufrgs.br>
2020-09-03 11:44:41 -0300

    SP-20 add reject promisse no método getById em activity-rest-service em caso de erro

--- source/app/activity-rest-service.js
+++ source/app/activity-rest-service.js
@@ -36,6 +36,7 @@
         defer.resolve(response.data);
       }).error(function (error) {
         console.error('Cannot GET a survey template.');
+        defer.reject(error);
       });
       return defer.promise;
     }
