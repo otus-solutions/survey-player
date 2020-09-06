d43b47673f2fb891336423a84f97306586dde4d7
FlaviaAP <flavia.avila@ufrgs.br>
2020-09-06 03:40:56 -0300

    SP-20 refatoração de survey-finish-participant-component-spec

--- source/tests/unit/app/otusjs-player-component/survey-finish-participant-component/survey-finish-participant-component-spec.js
+++ source/tests/unit/app/otusjs-player-component/survey-finish-participant-component/survey-finish-participant-component-spec.js
@@ -1,9 +1,7 @@
 describe('otusSurveyFinishParticipant component', function () {
+  let Mock = {};
   let Injections = [];
   let controller = {};
-
-  const REASON_TO_FINISH_PLAYER_WITH_SECONDARY_TEXT = 'ALREADY_FINALIZED';
-  const REASON_TO_FINISH_PLAYER_WITH_NO_SECONDARY_TEXT = 'IS_NOT_ME';
 
   beforeEach(function () {
     angular.mock.module('otusjs.player.standalone');
@@ -14,6 +12,8 @@ describe('otusSurveyFinishParticipant component', function () {
 
       controller = $controller('otusSurveyFinishParticipantCtrl', Injections);
     });
+
+    Mock.REASONS_TO_LIVE_PLAYER = Injections.PlayerService.getConstants().REASONS_TO_LIVE_PLAYER;
   });
 
   it('check controller', function () {
@@ -24,19 +24,22 @@ describe('otusSurveyFinishParticipant component', function () {
     expect(controller.$onInit).toBeDefined();
   });
 
-  it('onInit method should set message in case reasonToFinish has secondary text', function () {
-    _check_onInit_method_call(REASON_TO_FINISH_PLAYER_WITH_SECONDARY_TEXT);
-  });
+  describe('onInit method Suite Test', function () {
+    function _callOnInit(reason){
+      spyOn(Injections.PlayerService, 'getReasonToFinishActivity').and.returnValue(reason);
+      controller.$onInit();
+      expect(Injections.PlayerService.getReasonToFinishActivity).toHaveBeenCalledTimes(1);
+      expect(controller.message).toBeDefined();
+    }
 
-  it('onInit method should set longer message in case reasonToFinish has no secondary text', function () {
-    _check_onInit_method_call(REASON_TO_FINISH_PLAYER_WITH_NO_SECONDARY_TEXT);
-  });
+    it('onInit method should set message in case reasonToFinish has secondary text', function () {
+      _callOnInit(Mock.REASONS_TO_LIVE_PLAYER.ALREADY_FINALIZED);
+    });
 
-  function _check_onInit_method_call(reasonKey){
-    const reason = Injections.PlayerService.getConstants().REASONS_TO_LIVE_PLAYER[reasonKey];
-    spyOn(Injections.PlayerService, 'getReasonToFinishActivity').and.returnValue(reason);
-    controller.$onInit();
-    expect(controller.message).toBeDefined();
-  }
+    it('onInit method should set longer message in case reasonToFinish has no secondary text', function () {
+      _callOnInit(Mock.REASONS_TO_LIVE_PLAYER.IS_NOT_ME);
+    });
+
+  });
 
 });
