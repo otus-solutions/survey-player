b4f0f2bba03061f349d46718c29a044020cb6ec5
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-03 18:09:40 -0300

    SP-29 atualização de error-component-spec

--- source/tests/unit/app/otusjs-player-component/error/error-component-spec.js
+++ source/tests/unit/app/otusjs-player-component/error/error-component-spec.js
@@ -1,4 +1,4 @@
-describe('otusSurveyError component', function () {
+describe('otusSurveyErrorComponent_UnitTest_suite', function () {
   let Injections = [];
   let controller = {};
 
@@ -14,30 +14,48 @@ describe('otusSurveyError component', function () {
 
   });
 
-  it('check controller', function () {
+  it('check_controller', function () {
     expect(controller).toBeDefined();
   });
 
-  it('methods should defined', function () {
+  it('methods_should_defined', function () {
     expect(controller.$onInit).toBeDefined();
   });
 
-  describe('$onInit method Suite Test', function () {
-
-    it('onInit method should not set message in case callback address exists', function () {
-      spyOn(Injections.PlayerService, "hasCallbackAddress").and.returnValue(true);
-      controller.$onInit();
-      expect(Injections.PlayerService.hasCallbackAddress).toHaveBeenCalledTimes(1);
-      expect(controller.message).not.toBeDefined();
-    });
-
-    it('onInit method should set message in case callback address does not exists', function () {
+  describe('', () => {
+    it('onInit_method_should_set_message', function () {
       spyOn(Injections.PlayerService, "hasCallbackAddress").and.returnValue(false);
       controller.$onInit();
       expect(Injections.PlayerService.hasCallbackAddress).toHaveBeenCalledTimes(1);
       expect(controller.message).toBeDefined();
     });
 
+    it('onInit_method_should_set_showTryAgainButton_as_true_in_case_reasonToFinish_is_OFF_LINE_ERROR_and_has_not_callback', function () {
+      const REASON_TO_LIVE = Injections.PlayerService.getConstants().REASONS_TO_LIVE_PLAYER.ERROR_OFFLINE;
+      spyOn(Injections.PlayerService, "hasCallbackAddress").and.returnValue(false);
+      spyOn(Injections.PlayerService, "getReasonToFinishActivity").and.returnValue(REASON_TO_LIVE);
+      controller.$onInit();
+      expect(controller.showTryAgainButton).toBe(true);
+    });
+
+    it('onInit_method_should_set_showTryAgainButton_as_false_in_case_reasonToFinish_is_not_OFF_LINE_ERROR', function () {
+      const REASON_TO_LIVE = Injections.PlayerService.getConstants().REASONS_TO_LIVE_PLAYER.ERROR;
+      spyOn(Injections.PlayerService, "getReasonToFinishActivity").and.returnValue(REASON_TO_LIVE);
+      controller.$onInit();
+      expect(controller.showTryAgainButton).toBe(false);
+    });
+  });
+
+  it('goToCallback_method_should_call_PlayerService_goToCallback_method', function () {
+    spyOn(Injections.PlayerService, "goToCallback");
+    controller.goToCallback();
+    expect(Injections.PlayerService.goToCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('reloadSharedURL_method_should_call_PlayerService_reloadSharedURL_method', function () {
+    spyOn(Injections.PlayerService, "reloadSharedUrl");
+    controller.reloadSharedUrl();
+    expect(Injections.PlayerService.reloadSharedUrl).toHaveBeenCalledTimes(1);
   });
 
 });
