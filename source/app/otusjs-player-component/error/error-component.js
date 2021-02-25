558e8f3876ae8bcb081c6c1c7cf087413c3e846d
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-25 16:13:48 -0300

    SP-34 troca do valor default p/ reasonToFinish como UNEXPECTED_ERROR em error-component

--- source/app/otusjs-player-component/error/error-component.js
+++ source/app/otusjs-player-component/error/error-component.js
@@ -24,13 +24,12 @@
 
 
     function onInit() {
-      const OFF_LINE_ERROR = PlayerService.getConstants().REASONS_TO_LIVE_PLAYER.OFFLINE_ERROR;
-
-      const reasonToFinish = PlayerService.getReasonToFinishActivityAndClear() || OFF_LINE_ERROR;
+      const UNEXPECTED_ERROR = PlayerService.getConstants().REASONS_TO_LIVE_PLAYER.UNEXPECTED_ERROR;
+      const reasonToFinish = PlayerService.getReasonToFinishActivityAndClear() || UNEXPECTED_ERROR;
 
       self.participantSharedURLError = !PlayerService.hasCallbackAddress();
 
-      self.showTryAgainButton = (self.participantSharedURLError && reasonToFinish.id === OFF_LINE_ERROR.id);
+      self.showTryAgainButton = (self.participantSharedURLError && reasonToFinish.id === UNEXPECTED_ERROR.id);
 
       let template = `<md-icon md-font-set="material-icons" style="color: ${reasonToFinish.icon.color}">${reasonToFinish.icon.name}</md-icon>` +
         `<p class="md-display-1 shared-url-message-highlighted" style="color: ${reasonToFinish.highlightedText.color}">${reasonToFinish.highlightedText.text}</p>`;
